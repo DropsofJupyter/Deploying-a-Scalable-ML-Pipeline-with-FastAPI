ab67e00a82b0cf213d4bf32882a6e8fe885b385e
Jesse Q Morrow <jqtomorrow@Gmail.com>
2024-12-22 15:37:43 -0800

    successful test of test_two

--- test_ml.py
+++ test_ml.py
@@ -3,7 +3,8 @@ import pytest
 # TODO: add necessary import
 import pandas as pd
 import os
-
+from ml.model import load_model
+from sklearn.linear_model import LogisticRegression
 
 
 # TODO: implement the first test. Change the function name and input as needed
@@ -37,8 +38,13 @@ def test_two():
     """
     # add description for the second test
     """
-    # Your code here
-    pass
+    path = os.path.join(os.path.dirname(os.path.abspath(__file__)), "model", "model.pkl") # TODO: enter the path for the saved model 
+    model = load_model(path)
+
+    print(type(model))
+
+    print(isinstance(model, LogisticRegression))
+
 
 
 # TODO: implement the third test. Change the function name and input as needed
