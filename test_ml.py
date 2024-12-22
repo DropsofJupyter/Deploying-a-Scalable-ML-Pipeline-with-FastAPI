ac0f9808ee629b368017b03992b30fac22f2d504
Jesse Q Morrow <jqtomorrow@Gmail.com>
2024-12-22 15:45:53 -0800

    adds fully functional test_model_type() function with descr. and assert, also updated descr in first test.

--- test_ml.py
+++ test_ml.py
@@ -10,7 +10,7 @@ from sklearn.linear_model import LogisticRegression
 # TODO: implement the first test. Change the function name and input as needed
 def test_cat_features():
     """
-    check whether the categorical columns in the census.csv dataset match the categorical columns being transformed in the cat_features variable as it is instantiated in the main.py file at the project root.
+    check whether the categorical columns in the census.csv dataset from the project's data directory match the categorical columns being transformed in the cat_features variable as it is instantiated in the main.py file at the project root.
     """
     # Your code here
     project_path = os.path.dirname(os.path.abspath(__file__))
@@ -34,17 +34,14 @@ def test_cat_features():
     assert sorted(cat_features) == sorted(data_cat_features)
 
 # TODO: implement the second test. Change the function name and input as needed
-def test_two():
+def test_model_type():
     """
-    # add description for the second test
+    Loads the model pickle file from the model file of the project and ensures that it is, in fact, an instance of Sklearn.linear_model.LogisticRegression
     """
     path = os.path.join(os.path.dirname(os.path.abspath(__file__)), "model", "model.pkl") # TODO: enter the path for the saved model 
     model = load_model(path)
 
-    print(type(model))
-
-    print(isinstance(model, LogisticRegression))
-
+    assert isinstance(model, LogisticRegression)
 
 
 # TODO: implement the third test. Change the function name and input as needed
