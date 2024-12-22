439df759b41812148b20de8f972744096fb791c5
Jesse Q Morrow <jqtomorrow@Gmail.com>
2024-12-22 14:56:36 -0800

    add first test_one() function to test_ml.py

--- test_ml.py
+++ test_ml.py
@@ -1,4 +1,9 @@
 import pytest
+
+import pandas as pd
+import os
+
+
 # TODO: add necessary import
 
 # TODO: implement the first test. Change the function name and input as needed
@@ -7,8 +12,25 @@ def test_one():
     # add description for the first test
     """
     # Your code here
-    pass
+    project_path = os.path.dirname(os.path.abspath(__file__))
+    data_path = os.path.join(project_path, "data", "census.csv")
+    
+    data = pd.read_csv(data_path)
 
+    cat_features = [
+        "workclass",
+        "education",
+        "marital-status",
+        "occupation",
+        "relationship",
+        "race",
+        "sex",
+        "native-country",
+    ]
+
+    data_cat_features = data.select_dtypes(include="object").drop(columns='salary')
+
+    assert sorted(cat_features) == sorted(data_cat_features)
 
 # TODO: implement the second test. Change the function name and input as needed
 def test_two():
